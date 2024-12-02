ab569522b66cb33f550e72a8ae071b36b24e5fd6
EdgrantHS <edgrant.h@gmail.com>
2024-12-02 18:57:51 +0700

    Added cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
+const allowedOrigins = ["https://kemjar34.vercel.app"]; // Add other allowed origins if necessary
+// const allowedOrigins = ["http://localhost:3000"]; // Add other allowed origins if necessary
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: allowedOrigins.join(","), // Allows only the specified origins
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, PUT, DELETE", // Allow specific methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization", // Allow specific headers
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
